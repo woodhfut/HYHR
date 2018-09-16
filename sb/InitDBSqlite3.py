5f788528b6bec6542da229385fa8927965b9feeb
woodhfut <woodhfut@hotmail.com>
2018-09-16 22:03:06 +0800

    add db init script
    add db init script

---
+++ sb/InitDBSqlite3.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+db = sqlite3.connect('db.sqlite3')
+
+cur = db.cursor()
+
+#clear auth_user table
+cur.execute('delete from auth_user where username <> "sa"')
+
+#sb_district
+cur.execute('delete from sb_district')
+cur.execute('insert into sb_district("name") values ("北京丰台")')
+cur.execute('insert into sb_district("name") values ("北京海淀")')
+cur.execute('insert into sb_district("name") values ("北京朝阳")')
+cur.execute('insert into sb_district("name") values ("北京东城")')
+cur.execute('insert into sb_district("name") values ("北京西城")')
+cur.execute('insert into sb_district("name") values ("北京大兴")')
+cur.execute('insert into sb_district("name") values ("北京石景山")')
+cur.execute('insert into sb_district("name") values ("北京门头沟")')
+cur.execute('insert into sb_district("name") values ("北京昌平")')
+cur.execute('insert into sb_district("name") values ("北京顺义")')
+cur.execute('insert into sb_district("name") values ("北京怀柔")')
+cur.execute('insert into sb_district("name") values ("北京延庆")')
+cur.execute('insert into sb_district("name") values ("北京通州")')
+cur.execute('insert into sb_district("name") values ("北京平谷")')
+
+#sb_payment
+cur.execute('delete from sb_paymethod')
+cur.execute('insert into sb_paymethod("name") values("微信")')
+cur.execute('insert into sb_paymethod("name") values("支付宝")')
+cur.execute('insert into sb_paymethod("name") values("银行卡")')
+cur.execute('insert into sb_paymethod("name") values("淘宝")')
+
+#sb_ordertype
+cur.execute('delete from sb_ordertype')
+cur.execute('insert into sb_ordertype("name") values("正常缴费")')
+cur.execute('insert into sb_ordertype("name") values("年内补缴")')
+cur.execute('insert into sb_ordertype("name") values("跨年补缴")')
+cur.execute('insert into sb_ordertype("name") values("最近三个月补缴")')
+
+db.commit()
+cur.close()
+db.close()
